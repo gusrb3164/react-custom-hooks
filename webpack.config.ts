da8e34700f80541282c9ae7547b7531e7a58aeb4
hyeon-9yu <kim.h.1@chunmyung.com>
2021-09-09 22:42:48 +0900

    feat: add config settings

---
+++ webpack.config.ts
@@ -0,0 +1,51 @@
+import path from 'path';
+import webpack, { Configuration } from 'webpack';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+const config: Configuration = {
+  name: '@react-custom-hooks',
+  mode: isDevelopment ? 'development' : 'production',
+  devtool: !isDevelopment ? 'hidden-source-map' : 'eval',
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+    alias: {
+      '@src/*': path.resolve(__dirname, 'src'),
+    },
+  },
+  entry: {
+    app: './src/index.ts',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-typescript'],
+        },
+        exclude: path.join(__dirname, 'node_modules'),
+      },
+    ],
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+    }),
+    new webpack.EnvironmentPlugin({ NODE_ENV: isDevelopment ? 'development' : 'production' }),
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+    publicPath: '/dist/',
+  },
+};
+
+if (!isDevelopment && config.plugins) {
+  config.plugins.push(new webpack.LoaderOptionsPlugin({ minimize: true }));
+  config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'static' }));
+}
+
+export default config;
