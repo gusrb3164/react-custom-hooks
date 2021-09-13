03496a674bd1336ca7673171b447793f496796ab
hyeon-9yu <kim.h.1@chunmyung.com>
2021-09-13 23:33:27 +0900

    feat: add localstorage hook

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as useBooleanSetState } from './useBooleanSetState';
 export { default as useInput } from './useInput';
 export { default as useIntersectingState } from './useIntersectingState';
+export { default as useLocalStorage } from './useLocalStorage';
