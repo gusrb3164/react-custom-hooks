50ee7d1bee8714d52618c0016573d0bc0fe1bf69
hyeon-9yu <kim.h.1@chunmyung.com>
2021-10-13 23:55:39 +0900

    feat: add errorboundary hook

---
+++ src/useErrorBoundary.ts
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+export default function useErrorBoundary<ErrorType extends Error>() {
+  const [error, setError] = useState<ErrorType | null>(null);
+
+  if (error != null) {
+    throw error;
+  }
+
+  return setError;
+}
